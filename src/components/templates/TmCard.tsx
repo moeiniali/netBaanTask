e6482519e8d43159d1d1d7bb8089eed9b8255395
ali moeini <68547702+moeiniali@users.noreply.github.com>
2025-01-17 04:55:16 +0330

    Update TmCard.tsx

--- src/components/templates/TmCard.tsx
+++ src/components/templates/TmCard.tsx
@@ -8,7 +8,7 @@ type Props = {}
 
 const TmCard = (props: Props) => {
  const dispatch = useAppDispatch();
- const data: ServerData = useAppSelector((state) => state.cardSlice.data);
+ const data: ServerData = useAppSelector((state) => state.cardSlice.data.data);
  const loading: boolean = useAppSelector((state) => state.cardSlice.loading);
  const [selectedData, setSelectedData] = useState<Asset[]>([]);
 
@@ -16,32 +16,33 @@ const TmCard = (props: Props) => {
   dispatch(fetchData())
  }, [dispatch]);
 
+
+
+
+ const handleSelectData = useCallback((type: string) => {
+
+  const res = data?.assets?.filter((item) => {
+   return item.type === type
+  }
+
+  );
+  console.log(res);
+  
+  setSelectedData(res)
+ }, [data])
+
  useEffect(() => {
   handleSelectData('domain')
  }, [data])
 
-
- const handleSelectData = useCallback(async (event: any) => {
-  let type = event?.currentTarget?.id;
-  const res = data?.assets?.filter((item) => {
-   if (type) {
-    return item.type === type
-   } else {
-    return item.type === "domain"
-   }
-  }
-  );
-  setSelectedData(res)
- }, [data])
-
- console.log(loading);
+ console.log(selectedData);
 
 
  return (
   <div className={`${loading ? 'blur' : ''}  max-w-[1440px] m-auto h-full pt-20 p-4 flex  flex-row flex-wrap justify-center  xl:justify-between items-start gap-6`}>
-   <OrgDomain id="domain" onClick={handleSelectData} />
-   <OrgAddress id="ip" onClick={handleSelectData} />
-   <OrgCloud id="cloud" onClick={handleSelectData} />
+   <OrgDomain id="domain" onClick={()=>handleSelectData('domain')} />
+   <OrgAddress id="ip" onClick={()=>handleSelectData('ip')} />
+   <OrgCloud id="cloud" onClick={()=>handleSelectData('cloud')} />
 
    <OrgTable data={selectedData} />
 
