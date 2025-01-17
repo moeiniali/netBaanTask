8a74ef3241684ef46fcf55a553c8249b04ee4489
ali moeini <68547702+moeiniali@users.noreply.github.com>
2025-01-17 04:53:43 +0330

    Update fetchApi.ts

--- src/services/fetchApi.ts
+++ src/services/fetchApi.ts
@@ -2,7 +2,8 @@ import axios from "axios"
 
 export const fetchDate = async () => {
  try {
-  const response = await axios.post('https://run.mocky.io/v3/72e7e252-2f2b-462c-8e60-30a8a0cac801')
+  // const response = await axios.post('https://run.mocky.io/v3/72e7e252-2f2b-462c-8e60-30a8a0cac801')
+  const response = await axios.post('https://run.mocky.io/v3/0a346f61-45b6-4720-82e1-bb01ef4abed2')
   return response;
 
  }
